cc6de6f854f003d62e390d64e2314d9482473891
GitHub Action <action@github.com>
2022-01-27 22:26:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/mduffin95/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mduffin95/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mduffin95.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mduffin95.intellijplugin.MyBundle
 
 class MyApplicationService {
 
