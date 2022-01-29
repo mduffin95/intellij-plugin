9c53697eb1e4aa1528fd0991e8bea77861916539
Matthew Duffin <mduffin95@users.noreply.github.com>
2022-01-29 19:53:11 +0000

    Update

--- src/main/kotlin/com/github/mduffin95/intellijplugin/actions/JiraOpenInBrowserAction.kt
+++ src/main/kotlin/com/github/mduffin95/intellijplugin/actions/JiraOpenInBrowserAction.kt
@@ -10,21 +10,20 @@ import git4idea.annotate.GitFileAnnotation
 class JiraOpenInBrowserAction(name: String, private val annotation: FileAnnotation) : AnAction(name), UpToDateLineNumberListener {
 
     private var myLineNumber = -1
+    private val regex = Regex("^([A-Z]+-\\d+): .*\$")
 
     override fun actionPerformed(e: AnActionEvent) {
         if (myLineNumber < 0) return
 
         if (annotation !is GitFileAnnotation) return
 
-        val lineInfo = annotation.getLineInfo(myLineNumber)
-        val message = lineInfo?.subject!!
+        val lineInfo = annotation.getLineInfo(myLineNumber) ?: return
+        val message = lineInfo.subject
+        val matchResult = regex.find(message) ?: return
+        if (matchResult.groups.isEmpty()) return
+        val id = matchResult.groups[0]
 
-        val regex = Regex("^([A-Z]+-\\d+): .*\$")
-
-        val matchResult = regex.find(message)
-        val (id) = matchResult!!.destructured
         BrowserUtil.browse("https://opengamma.atlassian.net/browse/$id")
-        println(id)
     }
 
     override fun consume(t: Int) {
